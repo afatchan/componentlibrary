ec1500ce34be76f82006ec9cd6c4b76a81f3850e
Afat <13570123441>
2024-02-27 23:33:40 +0800

    remove dist/

--- dist/components/Select/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { SelectProps } from "./Select";
-import { OptionProps } from "./Option";
-export type ISelectComponent = FC<SelectProps> & {
-    Optiin: FC<OptionProps>;
-};
-declare const TranSelect: ISelectComponent;
-export default TranSelect;
--- dist/components/Select/index.js
+++
@@ -1,5 +0,0 @@
-import Select from "./Select";
-import Option from "./Option";
-var TranSelect = Select;
-TranSelect.Optiin = Option;
-export default TranSelect;
