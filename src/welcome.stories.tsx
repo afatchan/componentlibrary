664449004c1ca9fe2452932ab4e26762e35b230d
Afat <13570123441>
2024-03-05 17:57:11 +0800

    调整welcome页面的storybook

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -7,11 +7,23 @@ export default meta;
 type Story = StoryObj<typeof Welcome>;
 
 export const WelcomePage: Story = {
+  name:'welcome',
   render: () => (
     <>
       <h1>欢迎来到 @afat/componentLibrary 组件库</h1>
       <h3>安装试试</h3>
       <code>npm install @afat/componentLibrary --save</code>
+      <body>
+        <h3 style={{marginTop:'30px'}}>项目内容</h3>
+        <ul>
+          <li>typescript with React Hooks</li>
+          <li>使用 react-testing-library 完成单元测试</li>
+          <li>使用 storybook 本地调试和生成文档页面</li>
+          <li>使用 react-doc-gen 自动生成文档</li>
+          <li>使用第三方库扩充组件-(react-fontawesome, react-transition-group)</li>
+          <li>使用Sass完成组件样式</li>
+        </ul>
+      </body>
     </>
   ),
 };
